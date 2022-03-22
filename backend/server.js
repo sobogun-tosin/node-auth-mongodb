35298700345e052a1388ea1abd9191a8c505d4ae
sobogun-tosin <tosin.sobogun@trybrass.com>
2022-03-22 13:12:57 +0100

    added cors and updated dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv").config();
 const pageRoutes = require("./routes/pageRoutes");
 const blogRoutes = require("./routes/blogRoutes");
 const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 const { errorHandler } = require("./middleware/errorHandler");
 const connectDB = require("./config/db");
 const app = express();
@@ -16,6 +17,9 @@ app.set("views", "pages");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Allow cors for all request
+app.use(cors());
 
 // Routes
 app.use("/api/blogs", blogRoutes);
